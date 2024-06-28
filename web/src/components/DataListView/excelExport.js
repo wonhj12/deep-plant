062883be8b085c845e9609aedc3de4a9252a6f7e
{recon48} <{choe0920@naver.com}>
2024-06-28 11:36:09 +0900

    fix:error_handle_in_dashboard

--- web/src/components/DataListView/excelExport.js
+++ web/src/components/DataListView/excelExport.js
@@ -3,11 +3,18 @@ import { apiIP } from "../../config";
 
 // export 할 육류 데이터 목록 fetch
 const getDataListJSON = async () => {
-    const json = await (
-        await fetch(`http://${apiIP}/meat/get`)
-    ).json();
-   return json;
-};
+    try {
+      const response = await fetch(`http://${apiIP}/meat/get`);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const json = await response.json();
+      return json;
+    } catch (error) {
+      console.error('Fetch error:', error);
+      return null; // 또는 빈 객체나 배열을 반환할 수 있습니다.
+    }
+  };
 
 // JSON파일을 엑셀로 변환하기
 const downloadExcel = (data) => {
